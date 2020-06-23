cff63beeee3386692defaa3627da83922fd7f91b
diego-aquino <diego.cruz001@gmail.com>
2020-06-23 18:42:16 -0300

    File moved to './public'

---
+++ playground/public/client.js
@@ -0,0 +1,18 @@
+// import createKeyboardListener from './keyboardListener.js';
+// import createGame from './game.js';
+// import renderScreen from './renderScreen.js';
+
+// const game = createGame();
+
+// const screen = document.getElementById('screen');
+// renderScreen(screen, game, requestAnimationFrame);
+
+// const keyboardListener = createKeyboardListener(document);
+// keyboardListener.subscribe(game.movePlayer);
+
+// const socket = io();
+
+// socket.on('connect', () => {
+//     const playerId = socket.id;
+//     console.log(`PLayer connected on Client with id: ${playerId}`);
+// });
